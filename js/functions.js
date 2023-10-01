a5212f2c803895a9f8d37fb44381095dac6445a3
Темников Сергей <tems101104@gmail.com>
2023-10-01 21:39:38 +0500

    Task 2.29

---
+++ js/functions.js
@@ -0,0 +1,20 @@
+const lengthCheck = function (string, symbols) {
+  console.log(string.length <= symbols);
+};
+
+lengthCheck('абракадабра', 15);
+lengthCheck('абракадабра', 11);
+lengthCheck('абракадабра', 10);
+
+const palinCheck = function (string){
+  string = string.replaceAll(' ', '');
+  string = string.toLowerCase();
+  let emptyString = ('');
+  for (let i = string.length - 1; i >= 0; i = i - 1) {
+    let currSymb = string.at(i);
+    emptyString += currSymb;
+  }
+console.log(emptyString === string);
+};
+
+palinCheck('Лёша на полке клопа нашёл');
