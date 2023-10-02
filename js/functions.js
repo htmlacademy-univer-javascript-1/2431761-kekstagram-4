f554093c67a4282c77540309f93e9773e10119b6
BehindTheMemes <116211108+BehindTheMemes@users.noreply.github.com>
2023-10-02 15:57:33 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 const lengthCheck = function (string, symbols) {
-  console.log(string.length <= symbols);
+  return(string.length <= symbols);
 };
 
 lengthCheck('абракадабра', 15);
@@ -7,14 +7,13 @@ lengthCheck('абракадабра', 11);
 lengthCheck('абракадабра', 10);
 
 const palinCheck = function (string){
-  string = string.replaceAll(' ', '');
-  string = string.toLowerCase();
+  string = string.replaceAll(' ', '').toLowerCase();
   let emptyString = ('');
   for (let i = string.length - 1; i >= 0; i = i - 1) {
     let currSymb = string.at(i);
     emptyString += currSymb;
   }
-console.log(emptyString === string);
+return(emptyString === string);
 };
 
 
