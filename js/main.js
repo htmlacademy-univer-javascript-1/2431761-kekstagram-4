f84f4bab8c4ed2ff77994ea0521f2c98615c6f6a
BehindTheMemes <116211108+BehindTheMemes@users.noreply.github.com>
2023-10-20 20:31:59 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const names = ['Михаил',
+const Names = ['Михаил',
   'Денис',
   'Ярослав',
   'Ирина',
@@ -19,7 +19,7 @@ const names = ['Михаил',
   'Милана',
   'Анна',];
 
-const sentences = [
+const Sentences = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -28,7 +28,7 @@ const sentences = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const descriptions = [
+const Descriptions = [
   'Ласковый закат над океаном',
   'Расцветающее поле маков',
   'Мост через горную реку',
@@ -56,16 +56,16 @@ const descriptions = [
   'Рабочий на стройке, с отдыхающим веслом на плече',
 ];
 
-const avatarMin = 1;
-const avatarMax = 6;
+const AvatarMin = 1;
+const AvatarMax = 6;
 
-const photoCount = 25;
+const PhotoCount = 25;
 
-const likesMin = 15;
-const likesMax = 200;
+const LikesMin = 15;
+const LikesMax = 200;
 
-const numCommentsMin = 0;
-const numCommentsMax = 30;
+const NumCommentsMin = 0;
+const NumCommentsMax = 30;
 
 
 function getRandomInt(min, max) {
@@ -75,9 +75,9 @@ function getRandomInt(min, max) {
 function generateComment() {
   const comment = {
     id: getRandomInt(1, 1000), // Генерируем уникальный ID
-    avatar: `img/avatar-${getRandomInt(avatarMin, avatarMax)}.svg`, // Генерируем случайную аватарку
-    message: sentences[getRandomInt(0, sentences.length - 1)], // Выбираем случайное предложение
-    name: names[getRandomInt(0, names.length - 1)] // Выбираем случайное имя
+    avatar: `img/avatar-${getRandomInt(AvatarMin, AvatarMax)}.svg`, // Генерируем случайную аватарку
+    message: Sentences[getRandomInt(0, Sentences.length - 1)], // Выбираем случайное предложение
+    name: Names[getRandomInt(0, Names.length - 1)] // Выбираем случайное имя
   };
 
   return comment;
@@ -86,16 +86,16 @@ function generateComment() {
 function generatePhotosArray() {
   const photos = [];
 
-  for (let i = 1; i <= photoCount; i++) {
+  for (let i = 1; i <= PhotoCount; i++) {
     const photo = {
       id: i,
       url: `photos/${i}.jpg`,
-      description: descriptions[i],
-      likes: getRandomInt(likesMin, likesMax),
+      description: Descriptions[i],
+      likes: getRandomInt(LikesMin, LikesMax),
       comments: []
     };
 
-    const numComments = getRandomInt(numCommentsMin, numCommentsMax); // Генерируем случайное количество комментариев
+    const numComments = getRandomInt(NumCommentsMin, NumCommentsMax); // Генерируем случайное количество комментариев
 
     for (let j = 0; j < numComments; j++) {
       photo.comments.push(generateComment());
