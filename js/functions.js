350a162125c071158a60dc6afb92283363bfb7c9
Темников Сергей <tems101104@gmail.com>
2023-11-19 19:21:39 +0500

    5.16

--- js/functions.js
+++ js/functions.js
@@ -13,7 +13,7 @@ const palinCheck = function (string){
     let currSymb = string.at(i);
     emptyString += currSymb;
   }
-return emptyString === string;
+  return emptyString === string;
 };
 
 
@@ -21,3 +21,26 @@ palinCheck('топот');
 palinCheck('ДовОд');
 palinCheck('Кекс');
 palinCheck('Лёша на полке клопа нашёл');
+
+
+function meetingWithinWorkHours(dayStart, dayEnd, meetingStart, meetingDuration) {
+  function convertToMinutes(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  const dayStartInMinutes = convertToMinutes(dayStart);
+  const dayEndInMinutes = convertToMinutes(dayEnd);
+  const meetingStartInMinutes = convertToMinutes(meetingStart);
+  const meetingEndInMinutes = meetingStartInMinutes + meetingDuration;
+
+  return meetingStartInMinutes >= dayStartInMinutes && meetingEndInMinutes <= dayEndInMinutes;
+}
+
+// Примеры использования
+console.log(meetingWithinWorkHours('08:00', '17:30', '14:00', 90)); // true
+console.log(meetingWithinWorkHours('8:0', '10:0', '8:0', 120));     // true
+console.log(meetingWithinWorkHours('08:00', '14:30', '14:00', 90)); // false
+console.log(meetingWithinWorkHours('14:00', '17:30', '08:0', 90));  // false
+console.log(meetingWithinWorkHours('8:00', '17:30', '08:00', 900)); // false
+console.log(meetingWithinWorkHours('08:00', '15:30', '14:00', 90)); // true
